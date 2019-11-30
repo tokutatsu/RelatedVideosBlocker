fcfa99a4d648f815fa52aa960c6a6f73ab4186c5
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-30 21:22:49 +0900

    基本機能を実装した

---
+++ src/content_scripts/youtube.js
@@ -0,0 +1,33 @@
+// 関連動画などが表示されているブロック全体
+const secondary = document.getElementById('secondary');
+// 自動再生のトグル操作を行う要素
+const head = document.getElementById('head');
+// 次の動画と表示されるテキスト
+const upnext = document.getElementById('upnext');
+// 動画終了時に表示される関連動画
+const videos = document.querySelector('.videowall-endscreen');
+
+secondary.style.visibility = 'hidden';
+head.style.visibility = 'visible';
+upnext.style.visibility = 'hidden';
+videos.remove();
+
+// 広告の削除
+const removeAdvertisement = () => {
+    // 関連動画がある場所の広告
+    const playerAds = document.querySelectorAll('#player-ads');
+    // 動画内の広告
+    const videoAds = document.querySelectorAll('.video-ads');
+
+    for (const playerAd of playerAds) {
+        playerAd.remove();
+    }
+    for (const videoAd of videoAds) {
+        videoAd.remove();
+    }
+};
+
+
+window.addEventListener('load', () => {
+    removeAdvertisement();
+});
